8722683039731674da59200b9fe953501dc81e4c
Igelex <sergatam@yandex.com>
2017-05-02 17:59:09 +0200

    init Server and DB

---
+++ MS1/Prototype/Server/models_mongoose/users.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Pastuh on 02.05.2017.
+ */
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    gender: String,
+    birth_year: Number,
+    email: String,
+    pass: String,
+    picture: Number,
+    type: Number
+});
+
+module.exports = mongoose.model('users', userSchema);
---
+++ MS1/Prototype/Server/server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Pastuh on 02.05.2017.
+ */
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    controller_users = require('./javascript/controller_users');
+    port =  3000;
+
+mongoose.connect('mongodb://localhost/db');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('DB connected');
+});
+
+app.get('/', function (req, res) {
+    res.send('Hallo, suchki!!!')
+});
+
+app.post('/users', controller_users.addUser());
+
+app.listen(port, function () {
+    console.log('Server online');
+});
