e52795bf278eea77009039a4ef774e1027ebecc9
igelex <sergatam@yandex.com>
2017-05-20 17:40:50 +0200

    Rapid Prototype letzte anpassungen

--- MS1/Prototype/Client/HarvestHand/app/src/main/java/com/example/android/harvesthand/SignUp/SignUpActivity.java
+++ MS1/Prototype/Client/HarvestHand/app/src/main/java/com/example/android/harvesthand/SignUp/SignUpActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.harvesthand.SignUp;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
