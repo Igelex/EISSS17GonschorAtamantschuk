75f21f39f8f79a2a17f3eb46e08b5795e77a656b
franziska <franziska.gonschor@web.de>
2017-05-29 12:04:02 +0200

    Wetterdaten gefiltert

--- MS2/Prototype/Server/libs/javascript/weather.js
+++ MS2/Prototype/Server/libs/javascript/weather.js
@@ -10,7 +10,36 @@ module.exports.wetter = function (req, res) {
     request(requestUrl, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var result = JSON.parse(body);
-            console.log(result.current_observation.display_location.city);
+            console.log(result.current_observation.display_location.full);
+            console.log(result.current_observation.display_location.country);
+            console.log(result.current_observation.estimated.observation_time);
+            console.log(result.current_observation.estimated.local_time_rfc822);
+            console.log(result.current_observation.estimated.weather);
+            console.log(result.current_observation.estimated.temperature_string);
+            console.log(result.current_observation.estimated.temp_f);
+            console.log(result.current_observation.estimated.temp_c);
+            console.log(result.current_observation.estimated.relative_humidity);
+            console.log(result.current_observation.estimated.wind_string);
+            console.log(result.current_observation.estimated.pressure_mb);
+            console.log(result.current_observation.estimated.dewpoint_string);
+            console.log(result.current_observation.estimated.dewpoint_f);
+            console.log(result.current_observation.estimated.dewpoint_c);
+            console.log(result.current_observation.estimated.heat_index_string);
+            console.log(result.current_observation.estimated.heat_index_f);
+            console.log(result.current_observation.estimated.heat_index_c);
+            console.log(result.current_observation.estimated.windchill_string);
+            console.log(result.current_observation.estimated.feelslike_string);
+            console.log(result.current_observation.estimated.feelslike_f);
+            console.log(result.current_observation.estimated.feelslike_c);
+            console.log(result.current_observation.estimated.precip_today_string);
+            console.log(result.current_observation.estimated.precip_today_in);
+            console.log(result.current_observation.estimated.precip_today_metric);
+            console.log(result.current_observation.estimated.icon);
+            console.log(result.current_observation.estimated.icon_url);
+            console.log(result.current_observation.estimated.forecast_url);
+            console.log(result.current_observation.history_url);
+            console.log(result.current_observation.ob_url);
+
         } else {
             console.log(error, response.statusCode, body);
         }
